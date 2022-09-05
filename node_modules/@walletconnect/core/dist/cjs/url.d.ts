0fd01574efb88a09cd812bb9578a9021ca661dfd
Suhas Aggarwal <suhasagg@gmail.com>
2022-09-05 17:45:42 +0530

    Delete node_modules directory

--- node_modules/@walletconnect/core/dist/cjs/url.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function randomBridgeIndex(): number;
-export declare function selectRandomBridgeUrl(): string;
-export declare function getBridgeUrl(bridge: string): string;
-//# sourceMappingURL=url.d.ts.map
